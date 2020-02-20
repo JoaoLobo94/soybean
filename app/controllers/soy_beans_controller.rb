c8e2c8e27bd6b98cf51aaa8d1e50f69feade7a6d
joao lobo <joao.lobom@gmail.com>
2020-02-20 20:47:55 +0100

    importer working

--- app/controllers/soy_beans_controller.rb
+++ app/controllers/soy_beans_controller.rb
@@ -2,52 +2,63 @@
 
 require 'csv'
 class SoyBeansController < ApplicationController
-  def index; end
+  def index
+    import('/home/joao/Documents/code/tests/adcombi/soybean/storage/soybean-large.data')
+  end
 
   def show; end
 
   def find; end
 
-  def csv_cleaner
-    key_values_hash = { date: ['april', 'may', 'june', 'july', 'august', 'september', 'october', '?'],
-                        plant_stand: ['normal', 'lt-normal', '?'],
-                        precip: ['lt-norm,norm', 'gt-norm', '?'],
-                        temp: ['lt-norm,norm', 'gt-norm', '?'],
-                        hail: ['yes', 'no', '?'],
-                        crop_hist: ['diff-lst-year', 'same-lst-yr', 'same-lst-two-yrs', 'same-lst-sev-yrs', '?'],
-                        area_damaged: ['scattered', 'low-areas', 'upper-areas', 'whole-field', '?'],
-                        severity: ['minor', 'pot-severe', 'severe', '?'],
-                        seed_tmt: ['none', 'fungicide', 'other', '?'],
-                        germination: ['90-100%', '80-89%', 'lt-80%', '?'],
-                        plant_growth: ['norm', 'abnorm', '?'],
-                        leaves: %w[norm abnorm],
-                        leafspots_halo: ['absent', 'yellow-halos', 'no-yellow-halos', '?'],
-                        leafspots_marg: ["\tw-s-marg", 'no-w-s-marg', 'dna', '?'],
-                        leafspot_size: ['lt-1/8', 'gt-1/8', 'dna', '?'],
-                        leaf_shread: ['absent', 'present', '?'],
-                        leaf_malf: ['absent', 'present', '?'],
-                        leaf_mild: ['absent', 'upper-surf', 'lower-surf', '?'],
-                        stem: ['norm', 'abnorm', '?'],
-                        lodging: ['yes', 'no', '?'],
-                        stem_cankers: ['absent', 'below-soil', 'above-soil', 'above-sec-nde', '?'],
-                        canker_lesion: ['dna', 'brown', 'dk-brown-blk', 'tan', '?'],
-                        fruiting_bodies: ['absent', 'present', '?'],
-                        external_decay: ['absent', 'firm-and-dry', 'watery', '?'],
-                        mycelium: ['absent', 'present', '?'],
-                        int_discolor: ['none', 'brown', 'black', '?'],
-                        sclerotia: ['absent', 'present', '?'],
-                        fruit_pods: ['norm', 'diseased', 'few-present', 'dna', '?'],
-                        fruit_spots: ['absent', 'colored', 'brown-w/blk-specks', 'distort', 'dna', '?'],
-                        seed: ['norm', 'abnorm', '?'],
-                        mold_growth: ['absent', 'present', '?'],
-                        seed_discolor: ['absent', 'present', '?'],
-                        seed_size: ['norm', 'lt-norm', '?'],
-                        shriveling: ['absent', 'present', '?'],
-                        roots: ['norm', 'rotted', 'galls-cysts', '?'] }
-  end
+  def csv_cleaner; end
 
   def import(file)
-    CSV.parse(File.read(file), headers: false) do |soy|
+    key_values_hash = {
+      classification: ['diaporthe-stem-canker', 'charcoal-rot', 'rhizoctonia-root-rot',
+                       'phytophthora-rot', 'brown-stem-rot', 'powdery-mildew',
+                       'downy-mildew', 'brown-spot', 'bacterial-blight',
+                       'bacterial-pustule', 'purple-seed-stain', 'anthracnose',
+                       'phyllosticta-leaf-spot', 'alternarialeaf-spot',
+                       'frog-eye-leaf-spot', 'diaporthe-pod-&-stem-blight',
+                       'cyst-nematode', '2-4-d-injury', 'herbicide-injury'],
+      date: ['april', 'may', 'june', 'july', 'august', 'september', 'october', '?'],
+      plant_stand: ['normal', 'lt-normal', '?'],
+      precip: ['lt-norm,norm', 'gt-norm', '?'],
+      temp: ['lt-norm,norm', 'gt-norm', '?'],
+      hail: ['yes', 'no', '?'],
+      crop_hist: ['diff-lst-year', 'same-lst-yr', 'same-lst-two-yrs', 'same-lst-sev-yrs', '?'],
+      area_damaged: ['scattered', 'low-areas', 'upper-areas', 'whole-field', '?'],
+      severity: ['minor', 'pot-severe', 'severe', '?'],
+      seed_tmt: ['none', 'fungicide', 'other', '?'],
+      germination: ['90-100%', '80-89%', 'lt-80%', '?'],
+      plant_growth: ['norm', 'abnorm', '?'],
+      leaves: %w[norm abnorm],
+      leafspots_halo: ['absent', 'yellow-halos', 'no-yellow-halos', '?'],
+      leafspots_marg: ['tw-s-marg', 'no-w-s-marg', 'dna', '?'],
+      leafspot_size: ['lt-1/8', 'gt-1/8', 'dna', '?'],
+      leaf_shread: ['absent', 'present', '?'],
+      leaf_malf: ['absent', 'present', '?'],
+      leaf_mild: ['absent', 'upper-surf', 'lower-surf', '?'],
+      stem: ['norm', 'abnorm', '?'],
+      lodging: ['yes', 'no', '?'],
+      stem_cankers: ['absent', 'below-soil', 'above-soil', 'above-sec-nde', '?'],
+      canker_lesion: ['dna', 'brown', 'dk-brown-blk', 'tan', '?'],
+      fruiting_bodies: ['absent', 'present', '?'],
+      external_decay: ['absent', 'firm-and-dry', 'watery', '?'],
+      mycelium: ['absent', 'present', '?'],
+      int_discolor: ['none', 'brown', 'black', '?'],
+      sclerotia: ['absent', 'present', '?'],
+      fruit_pods: ['norm', 'diseased', 'few-present', 'dna', '?'],
+      fruit_spots: ['absent', 'colored', 'brown-w/blk-specks', 'distort', 'dna', '?'],
+      seed: ['norm', 'abnorm', '?'],
+      mold_growth: ['absent', 'present', '?'],
+      seed_discolor: ['absent', 'present', '?'],
+      seed_size: ['norm', 'lt-norm', '?'],
+      shriveling: ['absent', 'present', '?'],
+      roots: ['norm', 'rotted', 'galls-cysts', '?']
+    }
+
+   CSV.parse(File.read('/home/joao/Documents/code/tests/adcombi/soybean/storage/soybean-large.data'), headers: false, converters: :numeric) do |soy|
       data_dump = {
         classification: soy[0],
         date: soy[1],
@@ -86,7 +97,18 @@ class SoyBeansController < ApplicationController
         shriveling: soy[34],
         roots: soy[35]
       }
-      SoyBean.create!(data_dump)
+
+      corrected_data_dump = data_dump.each do |k, v|
+        key_values_hash.each do |k1, _v1|
+          if  v.is_a?(Integer)
+            data_dump[k] = key_values_hash[k1][v - 1] if k == k1
+          end
+        end
+      end
+
+      p  corrected_data_dump
+
+     # SoyBean.create!(corrected_data_dump)
     end
   end
 
