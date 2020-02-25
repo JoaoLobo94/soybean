2c451682758a1452ba7390e370cbb1e1c1271525
joao lobo <joao.lobom@gmail.com>
2020-02-25 11:45:28 +0100

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :soy_beans
-  root controller: :soy_beans, action: :index
-
+  resources :soy_beans, only: %i[index create]
 end
